96528dac900fa6367830d29e5d50d9412efa982d
nandv <vedantnn7.git@gmail.com>
2025-01-30 18:29:54 +0530

    setup theme and fonts

---
+++ modules/home/theme/default.nix
@@ -0,0 +1,21 @@
+# CREDITS: https://github.com/RGBCube/NCC/blob/master/options/theme.nix
+{ themes, lib, pkgs, ... }:
+let base = themes.raw.gruvbox-dark-hard;
+in {
+  options.theme = lib.mkValue (themes.custom (base // {
+    margin = 0;
+    padding = 6;
+
+    font.size.normal = 12;
+    font.size.big = 16;
+
+    font.sans.name = "Inter";
+    font.sans.package = pkgs.inter;
+
+    font.mono.name = "Iosevka Nerd Font";
+    font.mono.package = pkgs.nerd-fonts.iosevka;
+
+    icons.name = "Gruvbox-Plus-Dark";
+    icons.package = pkgs.gruvbox-plus-icons;
+  }));
+}
