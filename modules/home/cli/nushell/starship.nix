e10461c7ae767b88fd3fa67dd42395020843cccd
nandv <vedantnn7.git@gmail.com>
2025-01-31 14:49:32 +0530

    use % instead of right chevron for prompt

--- modules/home/cli/nushell/starship.nix
+++ modules/home/cli/nushell/starship.nix
@@ -2,6 +2,10 @@
   programs.starship = {
     enable = true;
     settings = {
+      character = {
+        success_symbol = "[%](bold green)";
+        error_symbol = "[%](bold red)";
+      };
       directory = {
         truncation_length = 8;
         truncate_to_repo = false;
