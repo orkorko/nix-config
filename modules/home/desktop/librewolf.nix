b8a617b8e554954ece3c641214b6d6b3236d3298
nandv <vedantnn7.git@gmail.com>
2025-02-01 21:08:50 +0530

    remove obsolete configs in liberwolf

--- modules/home/desktop/librewolf.nix
+++ modules/home/desktop/librewolf.nix
@@ -1,18 +1,12 @@
-{ config, pkgs, ... }: {
+{ config, ... }: with config.theme; {
   programs.librewolf = {
     enable = true;
     settings = {
       "general.autoScroll" = true;
       "privacy.donottrackheader.enabled" = true;
       "browser.fixup.domainsuffixwhitelist.idk" = true;
-      #"font.name.serif.x-western" = font.sans;
+      "font.name.serif.x-western" = font.sans.name;
       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
     };
-    # userChrome = ''
-    #     ${firefoxTheme}
-    #     #TabsToolbar {
-    #         visibility: collapse;
-    #     }
-    #     '';
   };
 }
