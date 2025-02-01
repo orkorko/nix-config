208942007682df56f5161f35bf7332c8fd271df1
nandv <vedantnn7.git@gmail.com>
2025-02-01 21:12:28 +0530

    configure nu declaratively and as a login shell

--- modules/home/cli/nushell/default.nix
+++ modules/home/cli/nushell/default.nix
@@ -1,36 +1,37 @@
-{ pkgs, ... }: {
+{ pkgs, lib, config, ... }: {
   imports = [ ./starship.nix ];
 
-  home.packages = [ pkgs.fish pkgs.carapace ];
-  
+  home.packages = with pkgs; [ fish carapace ];
+
   programs.nushell = {
     enable = true;
-
     shellAliases = {
       vim = "nvim";
       vi = "nvim";
       op = ''/home/abyss/code/openpilot/tools/op.sh "$@"'';
     };
-
+    settings = {
+      show_banner = false;
+      buffer_editor = "hx";
+      completions.external = {
+        enable = true;
+        max_results = 100;
+      };
+    };
+    environmentVariables = {
+      CARAPACE_BRIDGES = "fish,bash,inshellisense";
+    };
+    extraLogin = lib.mkIf config.isDesktop ''
+      if (tty) == '/dev/tty1' {
+         exec river
+      }
+    '';
     extraConfig = ''
       print "do improve win";
-
       let carapace_completer = {|spans|
           carapace $spans.0 nushell ...$spans | from json
       }
-
-      $env.config.completions.external = {
-          enable: true
-          max_results: 100
-          completer: $carapace_completer
-      }
-
-      $env.config.buffer_editor = "nvim";
-      $env.config.show_banner = false;
-
-      $env.CARAPACE_BRIDGES = 'fish,bash,inshellisense'
       source ~/.cache/carapace/init.nu
     '';
   };
-
 }
