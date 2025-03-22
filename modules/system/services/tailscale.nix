e39d7f834237719571f6ec8e204c5d01b0f6bc2f
nandv <heynandv@gmail.com>
2025-03-22 15:40:12 +0530

    setup tailscale

--- modules/system/services/tailscale.nix
+++ modules/system/services/tailscale.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }: {
-  services.tailscale.enable = true;
+{ pkgs, config, ... }: {
   environment.systemPackages = [ pkgs.tailscale ];
+  services.tailscale.enable = true;
+  networking.firewall = {
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ 50 config.services.tailscale.port ];
+  };
 }
