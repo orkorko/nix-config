dcbaf3545da6e01877b91b43226051482dc619e6
nandv <heynandv@gmail.com>
2025-03-22 15:57:19 +0530

    fix imports for home manager

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,6 +1,8 @@
-{ config, lib, username, stateVersion, ... }: {
-  imports = [ ./cli ./dev ./editors ./desktop ];
-
+{ isDesktop, lib, username, stateVersion, ... }: {
+  # @todo: imports are evaulated before options so this is a (temporary?)
+  # workaround for now
+  imports = [ ./cli ./dev ./editors ./services ]
+            ++ lib.optionals isDesktop [ ./desktop ];
   home = {
     inherit username stateVersion;
     homeDirectory = "/home/${username}";
