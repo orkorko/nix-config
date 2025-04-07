2dcc02abddc5b3d09db30891fa18f2804a6362b8
nandv <heynandv@gmail.com>
2025-04-07 22:04:21 +0530

    setup power management

--- modules/system/services/power-management.nix
+++ modules/system/services/power-management.nix
@@ -1,7 +1,25 @@
-{ config, ... }: with config; {
+{ lib, config, ... }: lib.mkIf config.isDesktop {
   services = {
-    thermald.enable = isDesktop;
-    power-profiles-daemon.enable = isDesktop;
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
   };
-  powerManagement.enable = isDesktop;
+  powerManagement.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 50;
+
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
 }
