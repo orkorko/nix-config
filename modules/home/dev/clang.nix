3a33b57f1cd643249d514af206a2f14560c07e79
nandv <heynandv@gmail.com>
2025-04-12 19:56:58 +0530

    add clang/gcc debuggers

--- modules/home/dev/clang.nix
+++ modules/home/dev/clang.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }: {
-  home.packages = [
-    pkgs.clang_16
-    pkgs.clang-tools_16
+  home.packages = with pkgs; [
+    clang_16
+    clang-tools_16
+    valgrind
+    gdb
+    lldb
+    linuxPackages.perf
+    # versions of ld required by gcc and clang conflict
+    # see: https://github.com/nix-community/home-manager/issues/1668#issuecomment-1264298055
+    (hiPrio gcc)
   ];
 }
